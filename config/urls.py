8e2ccb5abc6d354bbcf5bdb3042cdc8cf2feb423
= <hugolopezvalladares21@gmail.com>
2026-04-02 13:01:22 +0300

    Landing page

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', include('main.urls')),
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
