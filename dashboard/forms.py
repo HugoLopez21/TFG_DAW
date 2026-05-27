88547ea892cdc781b97d0bdf90952fda15a5d2aa
Hugo <hugolopezvalladares21@gmail.com>
2026-05-27 11:04:24 +0300

    Formularios y vistas para los empleados en el dahsboard de manager

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -4,21 +4,27 @@ from django import forms
 from django.urls import reverse
 
 
-class CustomUserCreationForm(UserCreationForm):
+
+class EmployeeCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ('email', 'password1', 'password2', 'first_name',
-                'last_name', 'date_of_birth')
+        fields = ('email', 'password1', 'first_name', 'last_name', 'role')
 
 
 # Filtro aplicado al formulario para evitar que se puedan modificar usuarios no empleados
-class CustomUserChangeForm(UserChangeForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        user_role = self.instance.role
-        if user_role != 'delivery_man' and user_role != 'employee':
-            raise forms.ValidationError("No se puede modificar este usuario")
-
+class EmployeeChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    # Clean para revisar si los datos son validos
+    def clean(self):   
+        cleaned_data = super().clean()
+        user_role = self.instance.role
+        if user_role != 'delivery_man' and user_role != 'employee':
+            raise forms.ValidationError("No se puede modificar este usuario")
+        return cleaned_data
+    
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import CustomUser
+from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
+from .forms import EmployeeChangeForm, EmployeeCreationForm
 # Create your views here.
 
 
@@ -13,6 +16,8 @@ from django.contrib.auth.decorators import user_passes_test
 # VISTA (Manager): Gestionar empleados, productos y men
 
 
+
+# COMPROBACIONES DE USUARIOS
 def is_manager(user):
     return user.role == "manager"
     
@@ -20,12 +25,59 @@ def is_employee(user):
     return user.role in ['delivery_man', 'employee']
 
 
-@user_passes_test(is_manager)
-def manager_dashboard(request):
-    return render(request, 'url')
+# RENDERIZA TEMPLATE DE LOS EMPLEADOS
+# Devolver la lista de usuarios empleados
+# Devolver la lista de productos filtrarlos para saber si estan en la carta o no
 
 @user_passes_test(is_employee)
 def orders_dahsboard(request):
     return render(request, 'url') 
 
+
+
+# RENDERIZA DASHBOAR DEL MANAGER
+@user_passes_test(is_manager)
+def manager_dashboard(request):
+    return render(request, 'url')
+
+
+
+@user_passes_test(is_manager)
+def create_employee(request):
+    if request.method == 'POST':
+        form = EmployeeCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Empleado creado con éxito.")
+            return redirect() # URL
+    else:
+        form = EmployeeCreationForm()
+    return render() # URL plantilla
+
+@user_passes_test(is_manager)
+def delete_employee(request, employee_id):
+    user = get_object_or_404(get_user_model(), id=employee_id)
+    
+    if is_manager(user):
+        user.delete()
+        messages.error(request, "No puedes eliminar al manager.")
+    else:
+        user.delete()
+        messages.success(request, "Usuario eliminado con éxito.")
         
+    return redirect() # añadir nombre url o vista
+
+@user_passes_test(is_manager)
+def modify_employee(request, employee_id):
+    user = get_object_or_404(get_user_model(), id=employee_id)
+    
+    if request.method == 'POST':
+        form = EmployeeChangeForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Empleado modificado con éxito.")
+            return redirect('panel_principal')
+    else:
+        form = EmployeeChangeForm(instance=user)
+        
+    return render() #URL
