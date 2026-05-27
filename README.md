d988f1121587ebfb7892f5138762a6fa3ed532fe
Hugo <hugolopezvalladares21@gmail.com>
2026-05-27 16:11:13 +0300

    Añadida configuracion react p

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # FINAL GRADE PROJECT
 Work in progress...
-
+Django + React compilado con Vite como islas independientes para el carrito y el estado del pedido
 test
---
+++ frontend-react/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // o @vitejs/plugin-react según la versión instalada
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // Ruta archivos compilados
+    outDir: path.resolve(__dirname, '../static/react'),
+    emptyOutDir: true,
+    // Configuración de la ruta de los paquetes react
+    rollupOptions: {
+      input: {
+        // Crear punto de entrada para cada componente asíncrono
+        cart: path.resolve(__dirname, 'src/react-modules/cart.jsx'),
+        tracker: path.resolve(__dirname, 'src/react-modules/tracker.jsx'),
+      },
+      output: {
+        // Forzamos nombres sin hashes aleatorios
+        entryFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+        chunkFileNames: '[name].js',
+      }
+    }
+  }
+})
