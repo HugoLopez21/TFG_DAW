15aa9a13ba86e53aea665cf065858dc5db707503
Hugo <hugolopezvalladares21@gmail.com>
2026-05-28 20:25:16 +0300

    Formulrio checkout cart

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,72 @@
 from django import forms
 from .models import Order
+from users.models import Address 
+import json
 
+# Formulario del checkout que recoge la información del cliente para el pedido
+class OrderCheckoutForm(forms.Form):
 
-# Formulario de checkout del cliente para tramitar el pedido
-class OrderCheckoutForm(forms.ModelForm):
-    class Meta:
-        pass
+    phone_number = forms.CharField(
+        max_length=15,
+        label="Número de teléfono",
+        widget=forms.TextInput(attrs={
+            'type': 'tel',
+            'pattern': '[0-9]+',
+        })
+    )
+
+    delivery_type = forms.ChoiceField(
+        choices=Order.DELIVERY_TYPE,
+        label="Elige el tipo de entrega",
+        initial='home', # Por defecto
+        widget=forms.RadioSelect()
+    )
+
+    address = forms.ModelChoiceField(
+        queryset=Address.objects.none(),  
+        label="Dirección de entrega",
+        required=False,  # False porque si eligen 'Pickup' no es necesario
+        empty_label="-- Selecciona una dirección guardada --",
+        widget=forms.Select()
+    )
+
+    notes = forms.CharField(
+        max_length=250,
+        required=False,
+        label="Notas para el pedido",
+        widget=forms.Textarea(attrs={
+            'placeholder': 'Introduce tus indicaciones personalizadas',
+        })
+    )
+
+    #Validador carrito
+    def clean_cart_data(self):
+        data = self.cleaned_data.get('cart_data')
+        try:
+            # Intentamos transformar el texto de React en una lista/objeto Python
+            cart_json = json.loads(data)
+        except (ValueError, TypeError):
+            raise forms.ValidationError("El formato del carrito no es válido.")
+            
+        # Si el JSON es válido pero está vacio
+        if not cart_json:
+            raise forms.ValidationError("Tu carrito está vacío. Añade productos antes de pagar.")
+            
+        return data
+
+    # guardará el JSON del LocalStorage
+    cart_data = forms.CharField(
+        widget=forms.HiddenInput(),
+        required=True
+    )
+    
+    #Validador que evita que si ha elegido a domicilio haya una direccion
+    def clean(self):
+        cleaned_data = super().clean()
+        delivery_type = cleaned_data.get('delivery_type')
+        address = cleaned_data.get('address')
+
+        if delivery_type == 'home' and not address:
+            raise forms.ValidationError("Si seleccionas envío a domicilio, debes elegir una dirección.")
+
+        return cleaned_data
