a6df785791003e7fa27a07d1e9d2f8e657af46cb
Hugo <hugolopezvalladares21@gmail.com>
2026-05-28 17:55:47 +0300

    Formulario eliminado de orders

--- orders/forms.py
+++ orders/forms.py
@@ -6,13 +6,3 @@ from .models import Order
 class OrderCheckoutForm(forms.ModelForm):
     class Meta:
         pass
-
-# Formulario para actualizar el estado del pedido por parte del personal
-class OrderStatusUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ['status']
-        widgets = {
-            # Este widget permite elegir una de las opciones de estado
-            'status': forms.RadioSelect(),
-        } 
