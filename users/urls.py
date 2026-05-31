b2131dba102bbf6df4bc7cafbd0b3b52319752d4
Hugo <hugolopezvalladares21@gmail.com>
2026-05-31 17:51:43 +0300

    Flujo panel usuario

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,9 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('profile/', views.user_profile, name='user_profile'),
+    path('profile/data/', views.profile_view, name='profile_data'),
+    path('profile/tracking/', views.tracking_view, name='tracking'),
+    path('profile/orders/', views.orders_history, name='orders_history'),
+    path('profile/catalog/', views.products_catalog_view, name='products_catalog'),
+    path('profile/suggestions/', views.suggestions_view, name='suggestions'),
 ]
--- users/views.py
+++ users/views.py
@@ -5,3 +5,27 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def user_profile(request):
     return render(request, 'users/profile.html')
+
+
+@login_required
+def profile_view(request):
+    return render(request, 'users/includes/profile_data.html')
+
+@login_required
+def tracking_view(request):
+    return render(request, 'users/includes/tracking.html')
+
+@login_required
+def products_catalog_view(request):
+    return render(request, 'users/includes/products_catalog.html')
+
+@login_required
+def orders_history(request):
+    return render(request, 'users/includes/orders_history.html')
+
+@login_required
+def suggestions_view(request):
+    if request.method == 'POST':
+        # Procesar el formulario normal de Django...
+        pass
+    return render(request, 'users/suggestions.html')
