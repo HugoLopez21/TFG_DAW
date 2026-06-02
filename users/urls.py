c5f0b2c244dfdab1b9558c604253cd0aefe0beba
Hugo <hugolopezvalladares21@gmail.com>
2026-06-02 15:15:19 +0300

    user data

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('profile/', views.user_profile, name='user_profile'),
-    path('profile/data/', views.profile_view, name='profile_data'),
+    path('profile/data/', views.user_data, name='user_data'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('profile/address/add/', views.add_address, name='add_address'),
     path('profile/tracking/', views.tracking_view, name='tracking'),
     path('profile/orders/', views.orders_history, name='orders_history'),
     path('profile/catalog/', views.products_catalog_view, name='products_catalog'),
--- users/views.py
+++ users/views.py
@@ -1,16 +1,65 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from orders.models import Order
+from .models import CustomUser
+from .forms import ProfileForm, AddressForm
+from django.shortcuts import redirect
+from django.contrib import messages
+
 
 # Create your views here.
+
+
+
+@login_required
+def edit_profile(request):
+    user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Perfil actualizado correctamente.')
+            return redirect('users:user_profile')
+        else:
+            messages.error(request, 'Corrige los errores del formulario.')
+            return render(request, 'users/includes/profile_data.html', {'form': form, 'user': user})
+    else:
+        form = ProfileForm(instance=user)
+        return render(request, 'users/includes/profile_data.html', {'form': form, 'user': user})
+
+
+@login_required
+def add_address(request):
+    user = request.user
+    if request.method == 'POST':
+        form = AddressForm(request.POST)
+        if form.is_valid():
+            address = form.save()
+            user.addresses.add(address)
+            messages.success(request, 'Dirección añadida correctamente.')
+            return redirect('users:user_profile')
+        else:
+            messages.error(request, 'Corrige los errores del formulario.')
+            return render(request, 'users/includes/profile_data.html', {'address_form': form, 'user': user})
+    return redirect('users:user_profile')
+
+
+
+
+#VISTAS DE RENDERIZACION DE PLANTILLAS
 @login_required
 def user_profile(request):
     return render(request, 'users/profile.html')
 
 
 @login_required
-def profile_view(request):
-    return render(request, 'users/includes/profile_data.html')
+def user_data(request):
+    if request.method == 'GET':
+        user = request.user
+        context = {
+            'user' : CustomUser.objects.get(id=user.id)
+        }
+        return render(request, 'users/includes/profile_data.html', context)
 
 @login_required
 def tracking_view(request):
